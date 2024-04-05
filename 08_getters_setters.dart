9d7b52417f4cc20483f91a8cb7c54a2144815455
Santa Cruz Andres <andres123stacruz@gmail.com>
2024-04-05 14:42:55 -0300

    Implementacion de getters y setter para propiedades customizadas

---
+++ 08_getters_setters.dart
@@ -0,0 +1,31 @@
+void main () {
+
+  final mySquare = Square ( side : 10);
+  mySquare.side = 5;
+print('El area es: ${ mySquare.area}');
+
+}
+
+class Square {
+    double _side; //side *side
+
+    Square({  required double side })
+    : _side = side;
+
+    double get area {
+      return  _side * _side;
+    }
+
+    set side( double value) {
+      print('setting new value $value');
+      if (  value <0  ) throw 'Value must be >=0';
+
+    _side = value;
+    
+    }
+
+    double calculateArea()  {
+      return _side * _side;
+    }
+    
+  }
