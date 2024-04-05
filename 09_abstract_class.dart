eabd93bbf4cb8a3f9cb329f221ceaee3e49ee33a
Santa Cruz Andres <andres123stacruz@gmail.com>
2024-04-05 16:36:58 -0300

    creacion de clases abstractas. S2C22

---
+++ 09_abstract_class.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  
+}
+
+ enum PlantType { nuclear, wind, water }
+
+abstract class EnergyPlant {
+  
+  double energyLeft;
+  PlantType type; //nuclear, wind, water
+  
+  EnergyPlant({
+    required this.energyLeft,
+    required this.type
+  });
+  
+  void consumeEnergy( double amount );
+  
+}
